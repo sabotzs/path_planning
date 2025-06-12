1fd12adda0930638c88962b0570d360524be98a3
sabotzs <georgi.kuklev@protonmail.com>
2025-06-12 23:05:25 +0300

    Add initial vector implementation

---
+++ src/lib/Vec2.ts
@@ -0,0 +1,45 @@
+export type Vec2 = {
+    x: number
+    y: number
+}
+
+export function Vec2(x: number, y: number): Vec2 {
+    return { x: x, y: y }
+}
+
+export function add(u: Vec2, v: Vec2): Vec2 {
+    return {
+        x: u.x + v.x,
+        y: u.y + v.y,
+    }
+}
+
+export function subtract(u: Vec2, v: Vec2): Vec2 {
+    return {
+        x: u.x - v.x,
+        y: u.y - v.y,
+    }
+}
+
+export function scale(v: Vec2, scalar: number): Vec2 {
+    return {
+        x: v.x * scalar,
+        y: v.y * scalar,
+    }
+}
+
+export function dot(u: Vec2, v: Vec2): number {
+    return u.x * v.x + u.y * v.y
+}
+
+export function cross(u: Vec2, v: Vec2): number {
+    return u.x * v.y - u.y * v.x
+}
+
+export function lengthSquared(v: Vec2): number {
+    return dot(v, v)
+}
+
+export function distanceSquared(u: Vec2, v: Vec2): number {
+    return lengthSquared(subtract(u, v))
+}
