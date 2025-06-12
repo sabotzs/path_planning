378e004dd3e75ea822240fff75136d44b20416a8
sabotzs <georgi.kuklev@protonmail.com>
2025-06-12 23:56:33 +0300

    Add drawing methods. Draw some polygons on the canvas.

---
+++ src/Draw.ts
@@ -0,0 +1,36 @@
+import { Polygon } from "./lib/Polygon"
+import { Vec2 } from "./lib/Vec2"
+
+export function drawPoint(ctx: CanvasRenderingContext2D, point: Vec2) {
+    ctx.beginPath()
+    ctx.arc(point.x, point.y, 4, 0, 2 * Math.PI)
+    ctx.fill()
+}
+
+export function drawLineSegment(
+    ctx: CanvasRenderingContext2D,
+    from: Vec2,
+    to: Vec2
+) {
+    ctx.beginPath()
+    ctx.moveTo(from.x, from.y)
+    ctx.lineTo(to.x, to.y)
+    ctx.stroke()
+}
+
+export function drawPolygon(ctx: CanvasRenderingContext2D, polygon: Polygon) {
+    if (polygon.points.length < 2) {
+        return
+    }
+
+    const last = polygon.points[polygon.points.length - 1]
+    ctx.beginPath()
+    ctx.moveTo(last.x, last.y)
+    polygon.points.forEach((point) => {
+        ctx.lineTo(point.x, point.y)
+        ctx.moveTo(point.x, point.y)
+    })
+    ctx.stroke()
+
+    polygon.points.forEach((point) => drawPoint(ctx, point))
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { drawPolygon } from "./Draw"
+import { Polygon } from "./lib/Polygon"
+import { Vec2 } from "./lib/Vec2"
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
-ctx.beginPath()
-ctx.moveTo(0, 0)
-ctx.lineTo(50, 50)
-ctx.stroke()
-ctx.closePath()
+window.addEventListener("load", () => {
+    draw()
+})
+
+// MARK: Objects
+// prettier-ignore
+const character = Polygon([
+  Vec2(50, 50),
+  Vec2(75, 50),
+  Vec2(75, 75),
+  // Vec2(50, 75),
+])
+
+// prettier-ignore
+const obstacles: Polygon[] = [
+  Polygon([
+    Vec2(150, 75),
+    Vec2(250, 75),
+    Vec2(200, 150)
+  ]),
+  Polygon([
+    Vec2(400, 500),
+    Vec2(500, 400),
+    Vec2(500, 500),
+  ]),
+]
+
+function draw() {
+    drawPolygon(ctx, character)
+    obstacles.forEach((obstacle) => drawPolygon(ctx, obstacle))
+}
