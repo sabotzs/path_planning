76496c5c502100454281f59473beb51e574ec210
sabotzs <georgi.kuklev@protonmail.com>
2025-06-17 00:37:15 +0300

    Add initial implementation of visibility line from single point.

---
+++ src/algorithms/VisibilityLine.ts
@@ -0,0 +1,111 @@
+import { AvlTree } from "@datastructures-js/binary-search-tree"
+import { cross3, distanceSquared, subtract, Vec2 } from "../models/Vec2"
+import { LineSegment } from "../models/LineSegment"
+import { approxEqVec, strictlyLess } from "./Float"
+import { angleComparePoints, castRay, distanceCompareSegments } from "./Utils"
+
+type EventType = "start" | "end"
+
+type Event = {
+    point: Vec2
+    segment: LineSegment
+    type: EventType
+}
+
+export function visibilityFromPoint(
+    origin: Vec2,
+    segments: LineSegment[]
+): Vec2[] {
+    const events: Event[] = []
+    const openEdges = new AvlTree<LineSegment>((a, b) =>
+        distanceCompareSegments(origin, a, b)
+    )
+    generateEventsAndOpenEdges(origin, segments, events, openEdges)
+    sortEvents(origin, events)
+    return processEvents(origin, events, openEdges)
+}
+
+function generateEventsAndOpenEdges(
+    origin: Vec2,
+    segments: LineSegment[],
+    events: Event[],
+    openEdges: AvlTree<LineSegment>
+) {
+    for (const segment of segments) {
+        const oab = cross3(origin, segment.a, segment.b)
+
+        if (strictlyLess(0, oab)) {
+            events.push({ point: segment.a, segment: segment, type: "start" })
+            events.push({ point: segment.b, segment: segment, type: "end" })
+        } else if (strictlyLess(oab, 0)) {
+            events.push({ point: segment.b, segment: segment, type: "start" })
+            events.push({ point: segment.a, segment: segment, type: "end" })
+        } else {
+            continue
+        }
+
+        const intersection = castRay(origin, Vec2(1, 0), segment)
+        if (
+            intersection &&
+            !approxEqVec(intersection, segment.a) &&
+            !approxEqVec(intersection, segment.b)
+        ) {
+            openEdges.insert(segment)
+        }
+    }
+}
+
+function sortEvents(origin: Vec2, events: Event[]) {
+    events.sort((first, second) => {
+        const a = first.point
+        const b = second.point
+        if (approxEqVec(a, b)) {
+            if (first.type === "end" && second.type === "start") {
+                return -1
+            } else if (first.type === "start" && second.type === "end") {
+                return 1
+            } else {
+                return 0
+            }
+        }
+        return angleComparePoints(origin, a, b)
+    })
+}
+
+function processEvents(
+    origin: Vec2,
+    events: Event[],
+    openEdges: AvlTree<LineSegment>
+): Vec2[] {
+    const visible: Vec2[] = []
+
+    for (const event of events) {
+        if (event.type === "end") {
+            openEdges.remove(event.segment)
+        }
+
+        if (openEdges.count() === 0) {
+            visible.push(event.point)
+        } else {
+            const nearest = openEdges.min()!.getValue()
+            const direction = subtract(event.point, origin)
+            const intersection = castRay(origin, direction, nearest)
+
+            if (!intersection) {
+                visible.push(event.point)
+            } else {
+                const distEventPoint = distanceSquared(origin, event.point)
+                const distIntersection = distanceSquared(origin, intersection)
+                if (strictlyLess(distEventPoint, distIntersection)) {
+                    visible.push(event.point)
+                }
+            }
+        }
+
+        if (event.type === "start") {
+            openEdges.insert(event.segment)
+        }
+    }
+
+    return visible
+}
