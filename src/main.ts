43d7140b30caf89cd93ced47ba0f0a0cf6a706e5
sabotzs <georgi.kuklev@protonmail.com>
2025-06-17 00:38:54 +0300

    Draw visibility points from the character points.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { drawPolygon } from "./drawing/Draw"
-import { Polygon } from "./models/Polygon"
+import { minkowskiSum } from "./algorithms/MinkowskiSum"
+import { visibilityFromPoint } from "./algorithms/VisibilityLine"
+import { drawLineSegment, drawPoint, drawPolygon } from "./drawing/Draw"
+import { offsetPolygon, Polygon, polygonToLineSegments } from "./models/Polygon"
 import { Vec2 } from "./models/Vec2"
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 window.addEventListener("load", () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
     draw()
 })
 
@@ -26,6 +30,11 @@ const obstacles: Polygon[] = [
     Vec2(200, 150)
   ]),
   Polygon([
+    Vec2(400, 125),
+    Vec2(450, 200),
+    Vec2(360, 150),
+  ]),
+  Polygon([
     Vec2(400, 500),
     Vec2(500, 400),
     Vec2(500, 500),
@@ -33,6 +42,22 @@ const obstacles: Polygon[] = [
 ]
 
 function draw() {
-    drawPolygon(ctx, character)
-    obstacles.forEach((obstacle) => drawPolygon(ctx, obstacle))
+    // drawPolygon(ctx, character)
+    // obstacles.forEach((obstacle) => drawPolygon(ctx, obstacle))
+    const pivot = character.points[0]
+    const minkowskiSums = obstacles
+        .map((obstacle) => minkowskiSum(character, obstacle))
+        .map((obstacle) => offsetPolygon(obstacle, pivot))
+    const lineSegments = minkowskiSums.flatMap((obstacle) =>
+        polygonToLineSegments(obstacle)
+    )
+
+    minkowskiSums.forEach((p) => console.log(p))
+
+    const origin = Vec2(300, 300)
+    const visible = visibilityFromPoint(pivot, lineSegments)
+
+    drawPoint(ctx, pivot)
+    minkowskiSums.forEach((obstacle) => drawPolygon(ctx, obstacle))
+    visible.forEach((point) => drawLineSegment(ctx, pivot, point))
 }
--- src/models/Polygon.ts
+++ src/models/Polygon.ts
@@ -1,4 +1,5 @@
-import { Vec2 } from "./Vec2"
+import { LineSegment } from "./LineSegment"
+import { add, Vec2 } from "./Vec2"
 
 export type Polygon = {
     points: Vec2[]
@@ -6,4 +7,16 @@ export type Polygon = {
 
 export function Polygon(points: Vec2[]): Polygon {
     return { points: points }
+}
+
+export function offsetPolygon(polygon: Polygon, t: Vec2): Polygon {
+    return Polygon(polygon.points.map((point) => add(point, t)))
+}
+
+export function polygonToLineSegments(polygon: Polygon): LineSegment[] {
+    const next = (i: number) => (i + 1) % polygon.points.length
+
+    return polygon.points.map((point, index) => {
+        return LineSegment(point, polygon.points[next(index)])
+    })
 }
