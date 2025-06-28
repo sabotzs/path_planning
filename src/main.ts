8032c120c734208eb7f489572220511c341bfc63
sabotzs <georgi.kuklev@protonmail.com>
2025-06-28 14:40:27 +0300

    Add button for creating targets.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { Vec2 } from "./models/Vec2"
 
 // MARK: State
 let isCreatingCharacter = false
+let isCreatingTarget = false
 
 // MARK: Objects
 const character = Polygon([])
+const target = Polygon([])
 
 // MARK: Elements
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
@@ -15,12 +17,15 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 const createCharacterButton = document.getElementById(
     "createCharacter"
 ) as HTMLButtonElement
+const createTargetButton = document.getElementById(
+    "createTarget"
+) as HTMLButtonElement
 
 // MARK: Events
 window.addEventListener("load", () => {
     canvas.width = canvas.clientWidth
     canvas.height = canvas.clientHeight
-    // draw()
+    draw()
 })
 
 canvas.addEventListener("click", (event) => {
@@ -31,6 +36,11 @@ canvas.addEventListener("click", (event) => {
         character.points.push(point)
         draw()
     }
+
+    if (isCreatingTarget) {
+        target.points.push(point)
+        draw()
+    }
 })
 
 createCharacterButton.addEventListener("click", (event) => {
@@ -39,7 +49,20 @@ createCharacterButton.addEventListener("click", (event) => {
     createCharacterButton.textContent = isCreatingCharacter
         ? "Creating character"
         : "Create character"
+
     if (!isCreatingCharacter) {
+        draw()
+    }
+})
+
+createTargetButton.addEventListener("click", (event) => {
+    isCreatingTarget = !isCreatingTarget
+    createTargetButton.classList.toggle("active", isCreatingTarget)
+    createTargetButton.textContent = isCreatingTarget
+        ? "Creating target"
+        : "Create target"
+
+    if (!isCreatingTarget) {
         draw()
     }
 })
@@ -47,6 +70,7 @@ createCharacterButton.addEventListener("click", (event) => {
 function draw() {
     ctx.reset()
     drawCharacter()
+    drawTarget()
 }
 
 function drawCharacter() {
@@ -58,5 +82,17 @@ function drawCharacter() {
         drawPath(ctx, character.points)
     } else {
         drawPolygon(ctx, character)
+    }
+}
+
+function drawTarget() {
+    const style = "rgb(231, 201, 36)"
+    ctx.strokeStyle = style
+    ctx.fillStyle = style
+
+    if (isCreatingTarget) {
+        drawPath(ctx, target.points)
+    } else {
+        drawPolygon(ctx, target)
     }
 }
